9a32be71adfb50c881b141d5e65dc7c2402b77cd
EzraDagang <ezradagang@gmail.com>
2026-04-25 17:26:02 +0800

    changed compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.mybusinessplus"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mybusinessplus"
